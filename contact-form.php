8a5cd1d6a6dc7580a40e41511f2f06651c680dfb
ariellouise <arielladders@gmail.com>
2022-04-17 08:28:30 -0400

    CNAME update

--- contact-form.php
+++ contact-form.php
@@ -1,7 +1,7 @@
 <?php
 // Configure your Subject Prefix and Recipient here
 $subjectPrefix = '[Contact via website]';
-$emailTo       = 'dev@ariellouise.dev';
+$emailTo       = 'arielladders@gmail.com';
 
 $errors = array(); // array to hold validation errors
 $data   = array(); // array to pass back data
